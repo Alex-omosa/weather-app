398f4e773a48c31d35834ba57944b6be4f59c420
alexomosa001@gmail.com <alexomosa001@gmail.com>
2020-02-25 07:46:58 +0300

    commits

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 window.addEventListener("load", () => {
   let lat;
   let long;
+  let temperatureDescription = document.getElementById(
+    "tempareture-description"
+  );
+  let temperatureDegree = document.getElementById("temperature-degree");
+  let locationTimeZone = document.getElementById("location-timezone");
+  let temperatureSection = document.querySelector(".temperature");
+  const temparetureSpan = document.querySelector(".temperature span");
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       lat = position.coords.latitude;
@@ -12,8 +19,34 @@ window.addEventListener("load", () => {
           return response.json();
         })
         .then(data => {
-          console.log(data);
+          const { temperature, summary, icon } = data.currently;
+          //DOM elements from API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimeZone.textContent = data.timezone;
+          // formula
+          let celsius = (temperature - 32) * (5 / 9);
+          //set icons
+          setIcon(icon, document.getElementById("icon1"));
+          // change to C or F
+
+          temperatureSection.addEventListener("click", () => {
+            if (temparetureSpan.textContent === "F") {
+              temparetureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temparetureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          });
         });
     });
+  }
+
+  function setIcon(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
   }
 });

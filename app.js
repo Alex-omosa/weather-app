42287f4537852b2bacb2d9c5e45b0566b3602f03
alexomosa001@gmail.com <alexomosa001@gmail.com>
2020-02-25 06:20:07 +0300

    commit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition(position => {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-
-      const api = `https://api.darksky.net/forecast/0dc7005a5e4d35619b754d6508ece30b/${lat},${long}`;
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/0dc7005a5e4d35619b754d6508ece30b/${lat},${long}`;
       fetch(api)
         .then(response => {
           return response.json();

6a3ff5156c02a6d99dac2470a54159e8ca54b1e0
alexomosa001@gmail.com <alexomosa001@gmail.com>
2020-02-25 06:08:07 +0300

    firdt

---
+++ app.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+  let lat;
+  let long;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      lat = position.coords.latitude;
+      long = position.coords.longitude;
+      const proxy = "https://cors-anywhere.herukuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/0dc7005a5e4d35619b754d6508ece30b/${lat},${long}`;
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        });
+    });
+  }
+});
